cf389d61fb14e9270db557694b70ec15ef85bbf3
johnfdz <108025077+johnfdz@users.noreply.github.com>
2022-10-25 15:03:43 -0500

    colores

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,9 @@ const toggleTheme = document.getElementById('toggle-theme');
 const toggleIcon = document.getElementById('toggle-icon');
 const toggleText = document.getElementById('toggle-text');
 
+const toggleColors = document.getElementById('toggle-colors');
+const rootStyles = document.documentElement.style;
+
 toggleTheme.addEventListener('click', ()=>{
     document.body.classList.toggle('dark');
     if(toggleIcon.src.includes('moon.svg')){
@@ -11,4 +14,8 @@ toggleTheme.addEventListener('click', ()=>{
         toggleIcon.src = 'assets/icons/moon.svg'
         toggleText.textContent = 'Dark Mode'
     }
-})
+});
+
+toggleColors.addEventListener('click',(e)=>{
+    rootStyles.setProperty('--primary-color', e.target.dataset.color);
+});
