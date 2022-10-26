ea81740e3e2cd3885cd8c9f7322f25babad65c48
johnfdz <108025077+johnfdz@users.noreply.github.com>
2022-10-26 13:45:11 -0500

    barras de progreso

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ const toggleTheme = document.getElementById('toggle-theme');
 const toggleIcon = document.getElementById('toggle-icon');
 const toggleText = document.getElementById('toggle-text');
 
+
 const toggleColors = document.getElementById('toggle-colors');
 const rootStyles = document.documentElement.style;
 
@@ -17,5 +18,7 @@ toggleTheme.addEventListener('click', ()=>{
 });
 
 toggleColors.addEventListener('click',(e)=>{
-    rootStyles.setProperty('--primary-color', e.target.dataset.color);
+    if(e.target.dataset.color!= null){
+        rootStyles.setProperty('--primary-color', e.target.dataset.color);
+    }    
 });
